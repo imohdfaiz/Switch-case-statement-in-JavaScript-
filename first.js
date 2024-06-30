b5f867862ae0cc4092485a90f6c365216e9703b8
imohdfaiz <immohdfaiz2@gmail.com>
2024-06-30 12:08:23 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,30 @@
+let day=66;
+let dayName;
+
+switch(day){
+    case 1 : 
+    dayName = 'monday';
+    break;
+    case 2 : 
+    dayName = 'tuesday';
+    break;
+    case 3 :
+        dayName = ' wednesday';
+        break;
+        case 4 :
+            dayName = 'thursday';
+            break;
+            case 5 :
+                dayName = 'friday';
+                break;
+                case 6 :
+                    dayName = 'saturday';
+                    break;
+                    case 7 : 
+                    dayName = 'sunday';
+                    break;
+                    default :
+                    dayName = 'invlid day';
+}
+
+console.log(dayName);
